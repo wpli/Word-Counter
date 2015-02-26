b186c894f73fb8be3a5edcb3b142d1b9cc7d586d
Rahul Bhargava <rahulb@media.mit.edu>
2015-02-26 10:13:02 -0500

    more basic files

---
+++ server.py
@@ -0,0 +1,20 @@
+import os, sys, time, json, logging, csv
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+
+app = Flask(__name__)
+
+# setup logging
+base_dir = os.path.dirname(os.path.abspath(__file__))
+logging.basicConfig(filename=os.path.join(base_dir,'wordcounter.log'),level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+@app.route("/")
+def index():
+    tag_sets = mediameter.tags.publicMediaTagSets()
+    return render_template("public-tags.html",
+        tag_sets = tag_sets
+    )
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
