8b7e684a628b2cb3a13b3dc98648ff96fd66804b
kanarinka <kanarinka@gmail.com>
2015-02-26 12:22:53 -0500

    file uploads work now

--- server.py
+++ server.py
@@ -4,10 +4,20 @@ import string
 from nltk import FreqDist
 from nltk.corpus import stopwords
 from operator import itemgetter
+import tempfile 
+import pprint
 
 from flask import Flask, render_template, jsonify, request, redirect, url_for
+from flask.ext.uploads import UploadSet, configure_uploads, TEXT
+
+TEMP_DIR = tempfile.gettempdir()
 
 app = Flask(__name__)
+# TODO: set max upload size
+app.config['UPLOADED_DOCS_DEST'] = TEMP_DIR
+
+docs = UploadSet('docs', TEXT)
+configure_uploads(app, (docs,))
 
 # setup logging
 base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -22,7 +32,19 @@ def index():
 
     #this means the form was submitted
 	if request.method == 'POST':	
-		bag_of_words = request.form['bagOfWords']
+
+		file_to_upload = request.files['fileOfWords']
+		if file_to_upload:
+			#TODO: handle extension error (flaskext.uploads.UploadNotAllowed exception)
+			filename = docs.save(file_to_upload)
+			filepath = os.path.join(TEMP_DIR,filename)
+			logger.debug("Saved file to %s" % filepath)
+			bag_of_words = ""
+			with open(filepath, "r") as myfile:
+				bag_of_words = myfile.read()
+		else:
+			bag_of_words = request.form['bagOfWords']
+
 		if "removeStopWords" in request.form:
 			remove_stop_words = request.form['removeStopWords']
 		else:
@@ -40,12 +62,8 @@ def index():
 		word_counts = sortCountList(countWords(words_perhaps_with_stop_words))
 
 		bigram_counts = sortCountList(countBigrams(words))
-		print bigram_counts
 
 		trigram_counts = sortCountList(countTrigrams(words))
-		print trigram_counts
-
-		print word_counts
 
 	return render_template("home.html", word_counts=word_counts, bigram_counts=bigram_counts, trigram_counts=trigram_counts)
 
@@ -75,7 +93,7 @@ def countTrigrams(words):
 
 def sortCountList(freqDist):
 	items = freqDist.items()
-	return sorted(items, key=itemgetter(1), reverse=True)
+	return sorted(items, key=itemgetter(1), reverse=True)[:40]
 
 if __name__ == "__main__":
     app.debug = True
