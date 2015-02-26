d42f1d449fcb74da2e58c7f97d6a64980e09a6c9
kanarinka <kanarinka@gmail.com>
2015-02-26 10:35:03 -0500

    more stuff

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import os, sys, time, json, logging, csv
 import nltk
+import string
 from nltk import FreqDist
 from nltk.corpus import stopwords
 
@@ -17,7 +18,9 @@ def index():
     #this means the form was submitted
 	if request.method == 'POST':
 		bagOfWords = request.form['bagOfWords']
-		fdist = FreqDist(bagOfWords)
+		words = nltk.tokenize.word_tokenize(bagOfWords)
+		words = [w for w in words if not w in stopwords.words('english') and not w in string.punctuation]
+		fdist = FreqDist(words)
 		print fdist.most_common(100)
 
 	return render_template("home.html")
