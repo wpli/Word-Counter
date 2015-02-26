b3c4e358bd63051b0b96e3c8c53416b7eabe0402
kanarinka <kanarinka@gmail.com>
2015-02-26 10:27:01 -0500

    updated stuff

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 import os, sys, time, json, logging, csv
+import nltk
+from nltk import FreqDist
+from nltk.corpus import stopwords
+
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 
 app = Flask(__name__)
@@ -8,10 +12,15 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 logging.basicConfig(filename=os.path.join(base_dir,'wordcounter.log'),level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET', 'POST'])
 def index():
-    # do some stuff
-    return render_template("home.html")
+    #this means the form was submitted
+	if request.method == 'POST':
+		bagOfWords = request.form['bagOfWords']
+		fdist = FreqDist(bagOfWords)
+		print fdist.most_common(100)
+
+	return render_template("home.html")
 
 if __name__ == "__main__":
     app.debug = True
